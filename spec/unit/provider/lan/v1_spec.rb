dcd8f09bb25f66134732cda9eefee67720a94bc5
nurfet-becirevic <nurfet@stackpointcloud.com>
2017-04-20 21:58:55 +0200

    add missing & update exisitng tests and mock data

--- spec/unit/provider/lan/v1_spec.rb
+++ spec/unit/provider/lan/v1_spec.rb
@@ -3,25 +3,63 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:lan).provider(:v1)
 
 describe provider_class do
-  context 'with the minimum params' do
+  context 'LAN operations' do
     before(:all) do
-      @resource = Puppet::Type.type(:lan).new(
+      @resource1 = Puppet::Type.type(:lan).new(
         datacenter_name: 'dummydc',
         name: 'lan1'
       )
-      @provider = provider_class.new(@resource)
+      @provider1 = provider_class.new(@resource1)
+
+      @resource2 = Puppet::Type.type(:lan).new(
+        datacenter_name: 'dummydc',
+        name: 'lan2',
+        public: true
+      )
+      @provider2 = provider_class.new(@resource2)
     end
 
     it 'should be an instance of the ProviderV1' do
-      expect(@provider).to be_an_instance_of Puppet::Type::Lan::ProviderV1
+      expect(@provider1).to be_an_instance_of Puppet::Type::Lan::ProviderV1
+      expect(@provider2).to be_an_instance_of Puppet::Type::Lan::ProviderV1
     end
 
-    context 'create' do
-      it 'should create ProfitBricks LAN' do
-        VCR.use_cassette('lan_create') do
-          expect(@provider.create).to be_truthy
-          expect(@provider.exists?).to be true
+    it 'should create ProfitBricks LAN with minimum params' do
+      VCR.use_cassette('lan_create_min') do
+        expect(@provider1.create).to be_truthy
+        expect(@provider1.exists?).to be true
+      end
+    end
+
+    it 'should create ProfitBricks LAN with all params' do
+      VCR.use_cassette('lan_create_all') do
+        expect(@provider2.create).to be_truthy
+        expect(@provider2.exists?).to be true
+      end
+    end
+
+    it 'should list LAN instances' do
+      VCR.use_cassette('lan_list') do
+        expect(provider_class.instances.length).to eq(2)
+      end
+    end
+
+    it 'should update public property of the LAN' do
+      VCR.use_cassette('lan_update') do
+        @provider2.public = false
+        updated_instance = nil
+        provider_class.instances.each do |instance|
+          updated_instance = instance if instance.name == 'lan2'
         end
+        expect(updated_instance.public).to eq(false)
+      end
+    end
+
+    it 'should delete LAN' do
+      VCR.use_cassette('lan_delete') do
+        expect(@provider2.destroy).to be_truthy
+        expect(@provider2.exists?).to be false
+        expect(provider_class.instances.length).to eq(1)
       end
     end
   end
--- spec/unit/provider/nic/v1_spec.rb
+++ spec/unit/provider/nic/v1_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 provider_class = Puppet::Type.type(:nic).provider(:v1)
 
 describe provider_class do
-  context 'with the minimum params' do
+  context 'NIC operations' do
     before(:all) do
       @resource = Puppet::Type.type(:nic).new(
         name: 'testnic',
         lan: 'lan1',
+        dhcp: true,
         datacenter_name: 'dummydc',
         server_name: 'testserver',
         firewall_active: true
@@ -19,12 +20,39 @@ describe provider_class do
       expect(@provider).to be_an_instance_of Puppet::Type::Nic::ProviderV1
     end
 
-    context 'create' do
-      it 'should create ProfitBricks NIC' do
-        VCR.use_cassette('nic_create') do
-          expect(@provider.create).to be_truthy
-          expect(@provider.exists?).to be true
+    it 'should create ProfitBricks NIC' do
+      VCR.use_cassette('nic_create') do
+        expect(@provider.create).to be_truthy
+        expect(@provider.exists?).to be true
+      end
+    end
+
+    it 'should list NIC instances' do
+      VCR.use_cassette('nic_list') do
+        expect(provider_class.instances.length).to eq(1)
+      end
+    end
+
+    it 'should update NIC' do
+      VCR.use_cassette('nic_update') do
+        @provider.dhcp = false
+        @provider.lan = 'lan2'
+        @provider.ips = ['208.94.36.74', '208.94.36.121']
+        updated_instance = nil
+        provider_class.instances.each do |instance|
+          updated_instance = instance if instance.name == 'testnic'
         end
+        expect(updated_instance.dhcp).to eq(false)
+        expect(updated_instance.lan).to eq('lan2')
+        expect(updated_instance.ips.length).to eq(2)
+        expect(updated_instance.ips).to include('208.94.36.74', '208.94.36.121')
+      end
+    end
+
+    it 'should delete NIC' do
+      VCR.use_cassette('nic_delete') do
+        expect(@provider.destroy).to be_truthy
+        expect(@provider.exists?).to be false
       end
     end
   end
